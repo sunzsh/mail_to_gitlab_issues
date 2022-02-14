9ffd2d5e2eee74ee0f3538387897dc634f209d07
sunzsh <cn.xiaoshan@gmail.com>
2022-02-15 02:09:26 +0800

    增加功能:可直接分配某人

--- glservice.js
+++ glservice.js
@@ -2,6 +2,7 @@ const request = require('request');
 const path = require('path');
 const fs = require('fs');
 const config = require('./config.js');
+const { resolve } = require('path');
 
 const BASE_URL = config.gitlab_base_url + "/api/v4";
 
@@ -33,28 +34,52 @@ function uploadfile(proj_id, gitlab_token, file, filename, contentType) {
 }
 
 
-
-function commitIssues(proj_id, gitlab_token, title, content) {
+function getAssigneeIdIfExists(gitlab_token, title) {
+    if (title == null) {
+        return Promise.resolve(null)
+    }
+    let assigneeName = title.replace(/.*@\s*(.*?)\s*$/g, '$1')
+    if (assigneeName === title) {
+        return Promise.resolve(null)
+    }
     return new Promise((resolve, reject) => {
-        var r = request.post(`${BASE_URL}/projects/${proj_id}/issues`, buildOptions(gitlab_token), (error, response, body) => {
+        request.get(`${BASE_URL}/users?search=${encodeURI(assigneeName)}`, buildOptions(gitlab_token), (error, response, body) => {
             if (error) {
                 reject(error);
                 return;
             }
-            resolve(JSON.parse(body));
+            let resArray = JSON.parse(body).filter(item => item.state === 'active')
+            if (resArray == null || resArray.length == 0) {
+                resolve(null) 
+            } else {
+                resolve(resArray[0].id)
+            }
         });
-        var form = r.form()
-        form.append('title', title);
-        form.append('description', content);
+    });
+}
+
+
+function commitIssues(proj_id, gitlab_token, title, content) {
+    return new Promise((resolve, reject) => {
+        getAssigneeIdIfExists(gitlab_token, title).then(assigneeId => {
+            let r = request.post(`${BASE_URL}/projects/${proj_id}/issues`, buildOptions(gitlab_token), (error, response, body) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                resolve(JSON.parse(body));
+            });
+
+            var form = r.form()
+            form.append('title', title.replace(/@.*$/g, ''))
+            form.append('description', content);
+            if (assigneeId) {
+                form.append('assignee_id', assigneeId)
+            }
+        })
     });
 }
 
 module.exports = { commitIssues, uploadfile };
 
-
-// commitIssues(442, '你好', "好好好！\n\n![test](/uploads/2f584fee7d5fd3f9fcf516e6585aaf58/test.png)").then(res => {
-//     console.log(res);
-// })
-// 442
-// **测试一下**\n\n![test](/uploads/2f584fee7d5fd3f9fcf516e6585aaf58/test.png)
 
